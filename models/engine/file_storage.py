c556314ce7a3d4c77d90b2344b3a47a3e07b042e
G-omar-H <omarghazi12@gmail.com>
2024-02-03 23:01:28 +0100

    hot fixes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -81,9 +81,11 @@ class FileStorage:
             id (int): id of the class instance
             cls (obj): class object_
         """
-        key = f"{cls.__name__}.{id}"
-        if key in self.__objects.keys():
-            return self.__objects[f"{cls.__name__}.{id}"]
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
         return None
 
     def count(self, cls=None):
