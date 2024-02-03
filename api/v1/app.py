93906fedb42608e91e42f9b2dd9eca0af95f518e
G-omar-H <omarghazi12@gmail.com>
2024-02-03 10:19:24 +0100

    3. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
 port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
@@ -33,5 +33,4 @@ def notfound(e):
 
 
 if __name__ == "__main__":
-
     app.run(host=host, port=port, threaded=True)
